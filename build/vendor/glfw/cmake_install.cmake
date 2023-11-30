ff3bf450223badc945493d2812b562f47190158f
yogilany <yousef.gilany021@eng-st.cu.edu.eg>
2023-11-30 07:05:00 +0200

    created new branch

--- build/vendor/glfw/cmake_install.cmake
+++ build/vendor/glfw/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/Ahmed Ameen/Computer Graphics/Project/CMPN205 Project - Student Version/vendor/glfw
+# Install script for directory: C:/Users/yahya/Documents/College/Graphics/graphics-project/vendor/glfw
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/GFX-LAB")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/GFX-LAB")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -32,9 +32,14 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "C:/msys64/mingw64/bin/objdump.exe")
+endif()
+
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("D:/Ahmed Ameen/Computer Graphics/Project/CMPN205 Project - Student Version/build/vendor/glfw/src/cmake_install.cmake")
+  include("C:/Users/yahya/Documents/College/Graphics/graphics-project/build/vendor/glfw/src/cmake_install.cmake")
 
 endif()
 
